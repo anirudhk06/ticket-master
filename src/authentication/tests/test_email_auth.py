c3814cdeafac7e5659312cfc0b669e8031e4a42b
anirudhk06 <anirudhani300@gmail.com>
2026-01-13 20:13:10 +0530

    add test cases for login and logout for jwt

--- src/authentication/tests/test_email_auth.py
+++ src/authentication/tests/test_email_auth.py
@@ -1,8 +1,14 @@
 from django.test import TestCase
 from rest_framework.test import APIRequestFactory
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.token_blacklist.models import BlacklistedToken
+from rest_framework_simplejwt.tokens import RefreshToken
 
-from authentication.views.email import SignUpAuthEndpoint
+from authentication.views.email import (
+    SignInAuthEndpoint,
+    SignOutAuthEndpoint,
+    SignUpAuthEndpoint,
+)
 from db.models import User
 
 
@@ -101,3 +107,83 @@ class TestSignUpAuthentication(TestCase):
         user = User.objects.get(email="user@test.com")
 
         self.assertEqual(user.last_login_medium, "email")
+
+
+class TestSignInAuthentication(TestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.authentication = JWTAuthentication()
+
+    def test_signin_success(self):
+        user = User.objects.create(email="user@test.com", last_login_medium="email")
+        user.set_password("StrongPass@123")
+        user.save()
+
+        request = self.factory.post(
+            "/auth/sign-in",
+            {"email": "user@test.com", "password": "StrongPass@123"},
+            format="json",
+        )
+
+        response = SignInAuthEndpoint.as_view()(request)
+
+        self.assertIn("access", response.data)
+        self.assertIn("refresh", response.data)
+        self.assertEqual(user.last_login_medium, "email")
+        self.assertEqual(user.pk, response.data["user_id"])
+        self.assertEqual(response.status_code, 200)
+
+    def test_wrong_password(self):
+        user = User.objects.create(
+            email="user@test.com",
+        )
+        user.set_password("StrongPass@123")
+        user.save()
+
+        request = self.factory.post(
+            "/auth/sign-in",
+            {"email": "user@test.com", "password": "WrongPass@123"},
+            format="json",
+        )
+
+        response = SignInAuthEndpoint.as_view()(request)
+
+        self.assertEqual(response.status_code, 400)
+
+    def test_signin_non_existent_user(self):
+        request = self.factory.post(
+            "/auth/sign-in",
+            {"email": "user@test.com", "password": "StrongPass@123"},
+            format="json",
+        )
+
+        response = SignInAuthEndpoint.as_view()(request)
+        self.assertEqual(response.status_code, 400)
+
+
+class TestSignOutAuthentication(TestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.authentication = JWTAuthentication()
+
+    def test_logout_success(self):
+        user = User.objects.create_user(
+            email="user@test.com",
+            username="user",
+            password="StrongPass@123",
+        )
+
+        refresh = RefreshToken.for_user(user)
+
+        factory = APIRequestFactory()
+        request = factory.post(
+            "/auth/sign-out/",
+            {"refresh": str(refresh)},
+            format="json",
+            HTTP_AUTHORIZATION=f"Bearer {str(refresh.access_token)}",
+        )
+
+        response = SignOutAuthEndpoint.as_view()(request)
+
+        self.assertEqual(response.status_code, 205)
+        self.assertEqual(BlacklistedToken.objects.count(), 1)
